b797bfb37bcb873adef6b362a8484a33273936e3
liuxx479 <liuxx479@gmail.com>
2018-02-22 16:04:13 -0500

    add bs to production run

--- nubias.py
+++ nubias.py
@@ -6,11 +6,9 @@ import WLanalysis
 import h5py
 from emcee.utils import MPIPool 
 
-Rarcmin = 2 ## arcmin smoothing
 Lbox = 512.0 #Mpc/h
 Ngrid = 256.0 ## the grid used for computing power spectrum
 ibins=arange(Ngrid)
-nn=(Lbox/Ngrid)**2
 Lgrid = Lbox/Ngrid
 
 idir = '/scratch/02977/jialiu/temp/'
@@ -67,42 +65,52 @@ def gridding (pos):
     grid = grid/mean(grid) - 1.0
     return grid
 
-def smoothing (grid, Rpix=Rarcmin*Ngrid/Lbox):
+def smoothing (grid, isteep=10.0, kcut=1.0, RMpc=0.0):
+    '''Smooth a 3D grid, using Logistic filter function. If RMpc is not 0, then use a Gaussian filter.
+    '''
+    kmax=2.0*pi/Lbox*Ngrid/2.0
     grid_fft = fftshift(fftpack.fftn(grid))
     z, y, x = np.indices(grid.shape)
     icenter=(x.max()-x.min())/2.0
     center = np.array([icenter, icenter, icenter])
     if grid.shape[0]%2 == 0:
         center+=0.5
-    d = Lbox/Ngrid
     freq_cube = sqrt((x - center[0])**2 + (y - center[1])**2 + (z - center[2])**2)
-    WR = exp(-(2*pi*freq_cube*Rpix)**2/2.0)
+    freq_cube *= 2*pi/Lbox 
+    if RMpc:
+        WR = exp(-(freq_cube*RMpc)**2/2.0)
+    else:
+        WR = 1-1.0/(1.0+exp(-isteep*log(freq_cube/kcut)))
+    WR[freq_cube>=kmax]=0.0 ### cut off at kmax
+    grid_fft *= WR
+    return ifftn(ifftshift(grid_fft*WR))
 
-    
-def ps (pos):
-    grid = gridding(pos)
-    k, ps3d = WLanalysis.PowerSpectrum3D(grid)
+def ps (grid_smoothed):
+    k, ps3d = WLanalysis.PowerSpectrum3D(grid_smoothed)
     return 2*pi*k/Lbox, ps3d*(Lgrid/Ngrid)**3
 
-def cross_ps (pos1, pos2):
-    grid1, grid2 = gridding(pos1), gridding(pos2)
-    k, ps3d = WLanalysis.CrossPowerSpectrum3D(grid)
+def bs (grid_smoothed):
+    k, ps3d = WLanalysis.CrossPowerSpectrum3D(grid_smoothed**2, grid_smoothed)
     return 2*pi*k/Lbox, ps3d*(Lgrid/Ngrid)**3
 
+def operation (pos):
+    grid_smoothed = smoothing(gridding(pos))
+    k,ips = ps(grid_smoothed)
+    return k, ips, bs(grid_smoothed)[1]
+
+mbins = [[12, 12.5, 13, 16]
 ## mcut = arange(11.0, 14.5, 0.5)
-def Phh_gen (cosmosnap, mcut = arange(11.0, 15.5, 0.5), mbins = arange(11, 16), dataset_name='Subsample', bins=50):    
-    '''compute Pmm, Phh (several cuts) for cosmo, snap
+def Phh_gen (cosmosnap, dataset_name='Subsample', bins=50):    
+    '''compute Pmm, Phh (several cuts, both ends binned, not just lower limit) for cosmo, snap
     '''
     cosmo, snap = cosmosnap
     subsample_fn = idir+cosmo+'/snapshots_subsample/snapshot_%03d_idmod_101_0.hdf5'%(snap)
     rockstar_fn = idir+cosmo+'/rockstar/out_%i.list'%(snap)
-    out_fn = '/work/02977/jialiu/nubias/Phh/Phh_%s_%03d.npy'%(cosmo, snap)
-    #outold_fn = '/work/02977/jialiu/nubias/Phh14/Phh_%s_%03d.npy'%(cosmo, snap)
-    out_arr = zeros(shape=(3+len(mcut)+len(mbins), bins)) ## k, Pmm, Phh, then various cuts
-    #out_arr = zeros(shape=(12+len(mbins), bins)) ## k, Pmm, Phh of N+1 bins
-#### fix bug
-    #out_arr [:10] = load (outold_fn)
-
+    out_fn = '/work/02977/jialiu/nubias/Ph2h/Ph2h_%s_%03d.npy'%(cosmo, snap)
+    out_arr = zeros(shape=(3+(len(mbins)-1)*2, bins)) 
+    ## old: k, Pmm, Phh, then various cuts
+    ## k, Pmm, Pm2m, Phh_bin1, Ph2h_bin1, Phh_bin2, Ph2h_bin2...
+    
     if not os.path.isfile(subsample_fn) or not os.path.isfile(rockstar_fn):
         ### skips if files do not exist for some reason
         print 'Warning: file not exist, cosmo, snap'
@@ -116,7 +124,7 @@ def Phh_gen (cosmosnap, mcut = arange(11.0, 15.5, 0.5), mbins = arange(11, 16), 
     dataset = f[dataset_name]
     particle_pos = dataset['Position']/1e3
     dataset=0 ## release memory
-    out_arr[:2] = ps(particle_pos)
+    out_arr[:2] = poeration(particle_pos)
     particle_pos=0 ## release memory
     
     ######### read rockstar files
@@ -126,26 +134,19 @@ def Phh_gen (cosmosnap, mcut = arange(11.0, 15.5, 0.5), mbins = arange(11, 16), 
     rock_pos = array([rock_arr['halo_x'],rock_arr['halo_y'],rock_arr['halo_z']]).T
     logM_arr = log10(rock_arr['halo_mvir'])
     rock_arr=0 ## release memory
+    Mhalo_max = amax(logM_arr) ## largest halo mass in catalogue
     
-    out_arr[2] = ps(rock_pos)[1]
-    
-    ######### apply Mlim to halos
-    jjj = 2
-    #jjj = 10
-    for imcut in mcut:
-        print 'Applying mass cut:', imcut, cosmo, snap        
-        if amax(logM_arr)<=imcut: ### no halo above this mass
-            break
-        out_arr[jjj] = ps(rock_pos[logM_arr>imcut])[1]
-        jjj += 1
     ### now do for binned masses, not Mlim
-    jjj=3+len(mcut)
-    for imbin in mbins:
-        if amax(logM_arr)<=imbin:
+    jjj=3
+    for ii in arange(len(mbins)-1):
+        iMmin, iMmax = mbins[ii],mbins[ii+1]
+        if Mhalo_max<=iMmin:
             break
-        print 'Applying mass bin:', imbin, imbin+1, cosmo, snap
-        out_arr[jjj] = ps(rock_pos[ (logM_arr>=imbin) & (logM_arr<imbin+1.0)])[1]
-        jjj+=1
+        print 'Applying mass bin:', iMmin, iMmax cosmo, snap
+        iout = operation(rock_pos[ (logM_arr>=iMmin) & (logM_arr<iMmax)])
+        out_arr[jjj] = iout[1] ## ps
+        out_arr[jjj+1] = iout[2] ## bs
+        jjj+=2
     save(out_fn,out_arr)
 
 def hmf_gen (cosmosnap, hist_bins=arange(10, 15.5, 0.1)):    
